e014a40cbd7cae337bbc7588b85f2161573a6d8b
Pedro <Pedro@Pedro-520>
2020-02-17 11:35:31 +0100

    Terminado TASCA 3

--- U4_T3/src/U4_Tarea3.java
+++ U4_T3/src/U4_Tarea3.java
@@ -1,10 +1,48 @@
 
 public class U4_Tarea3 {
-
+    
+    public static final String [] DIAS_SEMANA = {
+                                                 "Lunes",
+                                                 "Martes",
+                                                 "Miércoles",
+                                                 "Jueves",
+                                                 "Viernes",
+                                                 "Sabado",
+                                                 "Domingo"};
+    public static final int MAX_TRIANGULO = 5;
    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("prueba");
+        
+        //Creamos el objeto y apuntamos al metodo inicio
+        U4_Tarea3 programa = new U4_Tarea3();
+        programa.inicio();
     }
     
+    public static void inicio()
+    {
+        //Ejeción del programa               
+        mostrarDiesDeLaSetmana();
+        System.out.println("-----------");
+        dibuixTriangle();
+    }
+    
+    public static void mostrarDiesDeLaSetmana()
+    {
+        for(int i = 0; i < DIAS_SEMANA.length; i++)
+        {
+            System.out.println("- " + DIAS_SEMANA[i]);
+        }
+    }
+    
+    public static void dibuixTriangle()
+    {
+        for(int i = 0; i < MAX_TRIANGULO; i++)
+        {
+            for(int b = 0; b < i+1; b++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
