49bd2efe759efa2961ca5081b9806b548f51291c
Pedro <Pedro@Pedro-520>
2020-02-17 16:17:15 +0100

    Empezando TASCA 4

--- U4_T4/src/DiesMesPerDividir.java
+++ U4_T4/src/DiesMesPerDividir.java
@@ -4,6 +4,14 @@ public class DiesMesPerDividir {
     
     public static void main(String[] args) {
         
+        DiesMesPerDividir programa = new DiesMesPerDividir();
+        programa.inicio();
+        
+    }
+    
+    public void inicio()
+    {
+        
     }
     
 }
